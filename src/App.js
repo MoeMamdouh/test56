ad9ca0d2ede2de76ad7d55c9e2bef90bf1f740c4
MoeMamdouh <mohamed.mamdouh.do@gmail.com>
2019-07-11 18:33:53 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,37 @@
-import React from 'react';
-import { AppState, YellowBox, StatusBar } from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import { AppNavigator } from './navigators/App';
-import { initMixPanelSdk, rateApp, getRatingState } from './services';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import {
-  RATING_APP_TIMEOUT,
-  mixpanelEvents,
-  COLORS,
-  IS_ANDROID,
-} from './constants';
-import { isDateGreaterThan } from './utils';
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module']);
-import { Sentry } from 'react-native-sentry';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigators/App';
 
-if (!__DEV__)
-  Sentry.config(
-    'https://ac17ed60d8b74a879c6fc34ffa86b3bc@sentry.io/1282227',
-  ).install();
-
-// apearing rate alert time out
-const rateTimeOut = setTimeout(async () => {
-  const storedRateState = await getRatingState();
-  if (
-    storedRateState &&
-    storedRateState.ratingState == mixpanelEvents.RATING_LATER
-  ) {
-    if (
-      isDateGreaterThan(
-        storedRateState.date,
-        7 * storedRateState.laterClickCounts,
-        'days',
-      )
-    ) {
-      if (!__DEV__) rateApp();
-    }
-  } else {
-    if (!__DEV__) rateApp();
-  }
-}, RATING_APP_TIMEOUT);
-
-class App extends React.Component {
-  componentWillMount() {
-    initMixPanelSdk();
-  }
-
-  componentDidMount() {
-    StatusBar.setBarStyle('light-content');
-    IS_ANDROID ? StatusBar.setBackgroundColor(COLORS.statusbar_bluish) : null;
-    AppState.addEventListener('change', this._handleAppStateChange);
-  }
-
-  componentWillUnmount() {
-    AppState.removeEventListener('change', this._handleAppStateChange);
-  }
-
-  _handleAppStateChange = nextAppState => {
-    //if app go to background or inactive clear time out
-    if (!__DEV__ && nextAppState.match(/inactive|background/))
-      clearTimeout(rateTimeOut);
-  };
-
+type Props = {};
+export default class App extends Component<Props> {
   render() {
-    return (
-      <Provider store={store}>
-        <AppNavigator />
-      </Provider>
-    );
+    return <AppNavigator />;
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
