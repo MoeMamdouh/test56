8e7ac5fbe0ff3efc8d320bbbf3b77d7db1bcb111
MoeMamdouh <mohamed.mamdouh.do@gmail.com>
2019-07-11 18:33:25 +0200

    add test screens

---
+++ src/screens/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './style';
+
+export default class Home extends React.PureComponent {
+  render() {
+    return (
+      <View>
+        <Text>asdaskdas;ldakl;kd</Text>
+        <Text>asdaskdas;ldakl;kd</Text>
+        <Text>asdaskdas;ldakl;kd</Text>
+        <Text>asdaskdas;ldakl;kd</Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/Login/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../constants';
+
+export const styles = StyleSheet.create({
+  content: {
+    marginRight: 16,
+    marginLeft: 16,
+    marginTop: 32,
+  },
+});
